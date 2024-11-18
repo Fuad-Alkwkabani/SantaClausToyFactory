528d5cdda7cc9f873c70dd7fe26da96f16add25d
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-18 23:16:01 +0100

    craet the class goodtoy

--- src/main/java/models/GoodToy.java
+++ src/main/java/models/GoodToy.java
@@ -1,5 +1,41 @@
 package models;
 
-public class GoodToy {
+public class GoodToy extends Toy {
 
+    private String marca;
+    private String edadRecomendada;
+    private String categoria;
+    
+    public GoodToy(String id, String titulo, String tipo, String marca, String edadRecomendada, String categoria) {
+        super(id, titulo, tipo);
+        this.marca = marca;
+        this.edadRecomendada = edadRecomendada;
+        this.categoria = categoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getEdadRecomendada() {
+        return edadRecomendada;
+    }
+
+    public void setEdadRecomendada(String edadRecomendada) {
+        this.edadRecomendada = edadRecomendada;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    
 }
