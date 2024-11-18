bb28b837589c1e7ec30c9cf91f4e554e04bf3b7d
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-18 23:12:31 +0100

    craet the class toy

--- src/main/java/models/Toy.java
+++ src/main/java/models/Toy.java
@@ -1,5 +1,47 @@
 package models;
 
 public class Toy {
+    private String id;
+    private String titulo;
+    private String tipo;
+
+
+    public Toy(String id, String titulo, String tipo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.tipo = tipo;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    } 
+
+    
 
 }
