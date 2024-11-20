bafc4447920e309c71e80f11f9c02e944063aad9
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-20 11:29:31 +0100

    creat the class toy controller

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,5 +1,40 @@
 package controllers;
 
+import java.util.List;
+
+import models.Toy;
+import repository.ToyRepository;
+
 public class HomeController {
+    private ToyRepository toyRepository;
+
+    public HomeController(ToyRepository toyRepository) {
+        this.toyRepository = new ToyRepository(); 
+    }
+
+    public void addToy(Toy toy) {
+        toyRepository.addToy(toy);
+        System.out.println("Juguete añadido con éxito: " + toy);
+    }
+    public List<Toy> getAllToys() {
+        return toyRepository.getAllToys();
+    }
+
+    public List<Toy> getToysByType(String tipo) {
+        return toyRepository.getToysByType(tipo);
+    }
+    public boolean deleteToy(String id) {
+        boolean removed = toyRepository.deleteToyById(id);
+        if (removed) {
+            System.out.println("Juguete eliminado con éxito (ID: " + id + ")");
+        } else {
+            System.out.println("Juguete no encontrado (ID: " + id + ")");
+        }
+        return removed;
+    }
+
+    public String saveAllToysToString() {
+        return toyRepository.saveToString();
+    }
 
 }
