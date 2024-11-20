c370ba40cd07cd0455e41bd6777004b308569488
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-20 11:52:17 +0100

    creat the class homecontroller

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,6 +1,42 @@
 package controllers;
 
-
+import java.util.Scanner;
 
 public class HomeController {
-}
+     public String handleLogin() {
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Iniciar sesión de trabajo como:");
+        System.out.println("1. Elfo");
+        System.out.println("2. Santa Claus");
+        System.out.print("Seleccione una opción: ");
+
+        int option = Scanner.nextInt();
+        Scanner.nextLine(); 
+
+        if (option == 1) {
+            System.out.print("Ingrese la contraseña de Elfo: ");
+            String password = Scanner.nextLine();
+            if (password.equals("elf123")) {
+                return "Elf"; 
+            } else {
+                System.out.println("Contraseña incorrecta.");
+                return "Invalid";
+            }
+        } else if (option == 2) {
+            System.out.print("Ingrese la contraseña de Santa Claus: ");
+            String password = Scanner.nextLine();
+            if (password.equals("santa123")) {
+                return "Santa"; 
+            } else {
+                System.out.println("Contraseña incorrecta.");
+                return "Invalid";
+            }
+        } else {
+            System.out.println("Opción inválida.");
+            return "Invalid";
+        }
+    }
+
+    }
+    
